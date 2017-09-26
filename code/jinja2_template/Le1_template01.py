699e6c413a1662d27b313e268afe437e999f9125
ProHiryu <songheqi1996@gmail.com>
2017-09-26 18:49:34 +0800

    set templates

--- code/jinja2_template/Le1_template01.py
+++ code/jinja2_template/Le1_template01.py
@@ -1,11 +1,27 @@
 #-*-coding:utf8-*-
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'index'
+    class Person(object):
+        name = u'onetwo'
+        age = 18
+
+    p = Person()
+
+    context = {
+        'username':'oneone',
+        'gender':'female',
+        'age':23,
+        'person':p,
+        'websites':{
+            'baidu':'www.baidu.com',
+            'google':'www.google.com'
+        }
+    }
+    return render_template('index.html',**context)
 
 
 if __name__ == '__main__':
